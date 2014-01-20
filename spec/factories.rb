ab519f9074a828aba25c8245b358366a057c4c30
GanadiniAkshay <akshaykulkarni.2104@gmail.com>
2014-01-21 03:27:44 +0530

    Add sign in and signout

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    name     "Akshay Kulkarni"
-    email    "akshaykulkarni.2104@gmail.com"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
     password "foobar"
     password_confirmation "foobar"
+
+   factory :admin do
+      admin true
+   end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,10 +10,21 @@ describe User do
    it { should respond_to(:email) }
    it { should respond_to(:password_digest) }
    it { should respond_to(:password) }
+   it { should respond_to(:remember_token) }
    it { should respond_to(:password_confirmation) }
    it { should respond_to(:authenticate) }
 
    it { should be_valid}
+   it { should_not be_admin}
+
+   describe "with admin attribute set to 'true'" do
+     before do
+        @user.save!
+        @user.toggle!(:admin)
+     end
+  
+     it { should be_admin }
+   end
 
    describe "when name is not present" do
       before { @user.name= " " }
@@ -29,6 +40,11 @@ describe User do
        before { @user.name= "a"*51 }
        it { should_not be_valid }
    end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 
    describe "when email format is invalid" do
      it "should be invalid" do
